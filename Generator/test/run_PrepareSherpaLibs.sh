07f86d99586d6b8393b67680cb23c25a81e45704
SiewYan <siew.yan.hoh@cern.ch>
2018-06-10 21:03:32 +0200

    update

--- Generator/test/run_PrepareSherpaLibs.sh
+++ Generator/test/run_PrepareSherpaLibs.sh
@@ -2,4 +2,4 @@
 
 NAME="ZtoEE_0j_OpenLoops_13TeV"
 
-./PrepareSherpaLibs.sh -p ${NAME} -e SherpaGeneration/Generator/python/ExtendedSherpaWeights_cfi.py
+./PrepareSherpaLibs.sh -p ${NAME} -e SherpaGeneration/Generator/python/ExtendedSherpaWeights_Moriond18_cfi.py
