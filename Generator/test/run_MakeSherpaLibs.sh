f93a513bcb10b8af7de1b1e8a93cb30595bc62af
SiewYan <siew.yan.hoh@cern.ch>
2018-08-24 00:00:53 +0200

    Damn... you SUCK

--- Generator/test/run_MakeSherpaLibs.sh
+++ Generator/test/run_MakeSherpaLibs.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-ls Run.dat_*
-echo "Please enter the tag of available Run.dat: Format Run.dat_[TAG]"
-read NAME
-
-echo "./MakeSherpaLibs.sh -p ${NAME} -o LBCR -v -m mpirun -M '-n 9'"
-sleep 3
+NAME=$1
 
 ./MakeSherpaLibs.sh -p ${NAME} -o LBCR -v -m mpirun -M '-n 9'
--- Generator/test/run_PrepareSherpaLibs.sh
+++ Generator/test/run_PrepareSherpaLibs.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-NAME="ZtoEE_0j_OpenLoops_13TeV"
+#NAME="ZtoEE_0j_OpenLoops_13TeV"
+#SHERPA_CFI="ExtendedSherpaWeights_cfi.py"
+NAME=$1
+SHERPA_CFI=$2
 
 #./PrepareSherpaLibs.sh -p ${NAME} -e SherpaGeneration/Generator/python/ExtendedSherpaWeights_Moriond18_cfi.py
 
-./PrepareSherpaLibs.sh -p ${NAME} -e SherpaGeneration/Generator/python/ExtendedSherpaWeights_cfi.py
+./PrepareSherpaLibs.sh -p ${NAME} -e SherpaGeneration/Generator/python/${SHERPA_CFI}
