d7d04f4e44e853f738fc262147f05d60999267e2
SiewYan <siew.yan.hoh@cern.ch>
2018-09-13 15:45:37 +0200

    update comment

--- Generator/batch/run.sh
+++ Generator/batch/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-#Nevent is not needed (leave it any number) for generating only Sherpack
-./makeJob.sh ZtoEE_0j_OpenLoops_13TeV ExtendedSherpaWeights_cfi.py 2000 1nd
+#Nevent is preconfiged to 100 events for generating only Sherpack (The idea is to perform \
+#a whole generation chain to sort of validate the sherpack is working). Unless you wanna \
+#generate events, change 100 to desire sizable number.
+./makeJob.sh ZtoEE_0j_OpenLoops_13TeV ExtendedSherpaWeights_cfi.py 100 1nd
