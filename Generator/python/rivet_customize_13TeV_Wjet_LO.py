2a80b3ac15ede9a8b889049ec11084f1824321e2
SiewYan <siew.yan.hoh@cern.ch>
2019-01-31 20:56:58 +0100

    correction and suppression

--- Generator/python/rivet_customize_13TeV_Wjet_LO.py
+++ Generator/python/rivet_customize_13TeV_Wjet_LO.py
@@ -3,9 +3,9 @@ import FWCore.ParameterSet.Config as cms
 def customise(process):
         process.load('GeneratorInterface.RivetInterface.rivetAnalyzer_cfi')
 	#http://rivet.hepforge.org/analyses/CMS_2017_I1610623.html
-        process.rivetAnalyzer.AnalysisNames = cms.vstring('CMS_2017_CMS_2017_I1610623')
-	process.rivetAnalyzer.OutputFile = cms.string('mcfile_13TeV_Wjet_2jLO.yoda')
-	process.rivetAnalyzer.CrossSection = cms.double(1996)
+        #process.rivetAnalyzer.AnalysisNames = cms.vstring('CMS_2017_CMS_2017_I1610623')
+	#process.rivetAnalyzer.OutputFile = cms.string('mcfile_13TeV_Wjet_2jLO.yoda')
+	#process.rivetAnalyzer.CrossSection = cms.double(1996)
         process.generation_step+=process.rivetAnalyzer
         return(process)
 
