5d739de49c42cb7ad6ccce584f99a75a4875eef2
SiewYan <siew.yan.hoh@cern.ch>
2019-03-09 14:48:20 +0100

    checkpoin1

--- Generator/test/run_MakeSherpaLibs.sh
+++ Generator/test/run_MakeSherpaLibs.sh
@@ -2,6 +2,6 @@
 
 NAME=$1
 
-#./MakeSherpaLibs.sh -p ${NAME} -o LBCR -v -m mpirun -M '-n 9'
+./MakeSherpaLibs.sh -p ${NAME} -o LBCR -v -m mpirun -M '-n 9'
 
-./MakeSherpaLibs.sh -p ${NAME} -d ${CMSSW_BASE}/src/SherpaGeneration/Generator/sherpant -f drellyanZp -o LBCR -v -m mpirun -M '-n 9'
+#./MakeSherpaLibs.sh -p ${NAME} -d ${CMSSW_BASE}/src/SherpaGeneration/Generator/sherpant -f drellyanZp -o LBCR -v -m mpirun -M '-n 9'
