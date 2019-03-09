2ead3110bb3bf71d9987c62ccd82d574c5cffc59
SiewYan <siew.yan.hoh@cern.ch>
2019-03-09 14:58:23 +0100

    remove weight

--- Generator/test/run_PrepareSherpaLibs.sh
+++ Generator/test/run_PrepareSherpaLibs.sh
@@ -3,4 +3,5 @@
 NAME=$1
 SHERPA_CFI=$2
 
-./PrepareSherpaLibs.sh -p ${NAME} -e SherpaGeneration/Generator/python/${SHERPA_CFI}
+#./PrepareSherpaLibs.sh -p ${NAME} -e SherpaGeneration/Generator/python/${SHERPA_CFI}
+./PrepareSherpaLibs.sh -p ${NAME}
