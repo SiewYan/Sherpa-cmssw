1590b9588ca2aab9014a20c2990d51f8a9b8e011
SiewYan <siew.yan.hoh@cern.ch>
2019-03-19 20:12:27 +0100

    update

--- Generator/fetchSherpa.sh
+++ Generator/fetchSherpa.sh
@@ -10,21 +10,21 @@ rm SHERPA-MC-2.2.5.tar.gz
 
 echo "Remember to remove manual folder and manual in Makefile"
 
-echo "
-Libraries have been installed in:
-   /lustre/cmswork/hoh/GenStudy/CMSSW_9_3_13/src/SherpaGeneration/Generator/sherpant/lib
 
-If you ever happen to want to link against installed libraries
-in a given directory, LIBDIR, you must either use libtool, and
-specify the full pathname of the library, or use the `-LLIBDIR'
-flag during linking and do at least one of the following:
-   - add LIBDIR to the `LD_LIBRARY_PATH' environment variable
-     during execution
-   - add LIBDIR to the `LD_RUN_PATH' environment variable
-     during linking
-   - use the `-Wl,-rpath -Wl,LIBDIR' linker flag
-   - have your system administrator add LIBDIR to `/etc/ld.so.conf'
+#echo "
+#Libraries have been installed in:
+#   /lustre/cmswork/hoh/GenStudy/CMSSW_9_3_13/src/SherpaGeneration/Generator/sherpant/lib
 
-See any operating system documentation about shared libraries for
-more information, such as the ld(1) and ld.so(8) manual pages.
-"
+#If you ever happen to want to link against installed libraries
+#in a given directory, LIBDIR, you must either use libtool, and
+#specify the full pathname of the library, or use the `-LLIBDIR'
+#flag during linking and do at least one of the following:
+#   - add LIBDIR to the `LD_LIBRARY_PATH' environment variable
+#     during execution
+#   - add LIBDIR to the `LD_RUN_PATH' environment variable
+#     during linking
+#   - use the `-Wl,-rpath -Wl,LIBDIR' linker flag
+#   - have your system administrator add LIBDIR to `/etc/ld.so.conf'
+#See any operating system documentation about shared libraries for
+#more information, such as the ld(1) and ld.so(8) manual pages.
+#"
--- Generator/test/cmsCreate.sh
+++ Generator/test/cmsCreate.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+frag=$1
+
+Nevent="1000"
+
 echo "
-cmsDriver.py SherpaGeneration/Generator/python/sherpa_HAHM_variableMW_UFO_Zpmumu_LO_13TeV_MASTER_cff.py \
-    -s GEN -n 1000 --no_exec --conditions auto:mc --eventcontent RAWSIM
+cmsDriver.py SherpaGeneration/Generator/python/${frag} \
+--mc -s GEN --datatier GEN-SIM -n ${Nevent} --no_exec --conditions auto:run2_mc_FULL \
+--eventcontent RAWSIM --beamspot Realistic25ns13TeVEarly2017Collision
 "
-cmsDriver.py SherpaGeneration/Generator/python/sherpa_HAHM_variableMW_UFO_Zpmumu_LO_13TeV_MASTER_cff.py \
-    -s GEN -n 1000 --no_exec --conditions auto:mc --eventcontent RAWSIM
+
+cmsDriver.py SherpaGeneration/Generator/python/${frag} \
+    --mc -s GEN --datatier GEN-SIM -n ${Nevent} --no_exec --conditions auto:run2_mc_FULL \
+    --eventcontent RAWSIM --beamspot Realistic25ns13TeVEarly2017Collision
